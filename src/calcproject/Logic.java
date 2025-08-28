2786c127adcec8d414938c94f52218496c0c4336
Ali Delgarm <delgarm10@gmail.com>
2025-08-28 11:02:57 +0330

    bugs fixes

--- src/calcproject/Logic.java
+++ src/calcproject/Logic.java
@@ -32,23 +32,55 @@ public class Logic {
         uif.getroundButton13().addActionListener(e -> result());
     }
     
+    private String handleMulP(String text) {
+        if(text.endsWith(")")){
+            return text + "*";
+        }else{
+        return text;
+        }
+    }
+    
+    private String lastNumber(String text) {
+        int lastOpIndex = -1;
+        for(int i = text.length() - 1; i >= 0; i--) {
+            if("+-*/%".indexOf(text.charAt(i)) >= 0) {
+                lastOpIndex = i;
+                break;
+            }
+        }
+        if(lastOpIndex >= 0){
+            return text.substring(lastOpIndex + 1);
+        }else{
+            return text;
+        }
+    }
+    
     private void appendNumber(String num) {
-        String text = uif.getjTextField3().getText();
-        if (isTypingNewNumber || text.equals("0")) {
+        String text = handleMulP(uif.getjTextField3().getText());
+        if(isTypingNewNumber || text.equals("0")) {
             uif.getjTextField3().setText(num);
             isTypingNewNumber = false;
-        }else {
+        }else{
             uif.getjTextField3().setText(text + num);
         }
     }
-
+    
     private void appendDot() {
-        String text = uif.getjTextField3().getText();
-        if (!text.contains(".")) {
+        String text = handleMulP(uif.getjTextField3().getText());
+        String lastNumber = lastNumber(text);
+
+        if(lastNumber.isEmpty()){
+            uif.getjTextField3().setText(text + "0.");
+            isTypingNewNumber = false;
+            return;
+        }
+
+        if(!lastNumber.contains(".")){
             uif.getjTextField3().setText(text + ".");
             isTypingNewNumber = false;
         }
     }
+    
 
     private void setOperator(String op) {
         String text = uif.getjTextField3().getText();
@@ -67,212 +99,165 @@ public class Logic {
         isTypingNewNumber = true;
     }
 
-//    private void sign() {
-//        String text = uif.getjTextField3().getText();
-//        if(text.equals("0")){
-//            return;
-//        }
-//        int lastOpIndex = -1;
-//        for(int i = text.length() - 1; i >= 0; i--) {
-//            if("+-*/%".indexOf(text.charAt(i)) >= 0) {
-//                lastOpIndex = i;
-//                break;
-//            }
-//        }
-//
-//        String prefix;
-//        String lastNumber;
-//        if(lastOpIndex >= 0){
-//            prefix = text.substring(0, lastOpIndex + 1);
-//            lastNumber = text.substring(lastOpIndex + 1);
-//        }else{
-//            prefix = "";
-//            lastNumber = text;
-//        }
-//
-//        if(!lastNumber.isEmpty()){
-//            if(lastNumber.startsWith("(-") && lastNumber.endsWith(")")) {
-//                lastNumber = lastNumber.substring(2, lastNumber.length() - 1);
-//            }else{
-//                lastNumber = "(-" + lastNumber + ")";
-//            }
-//        }
-//
-//        uif.getjTextField3().setText(prefix + lastNumber);
-//    }
-    
     private void sign() {
         String text = uif.getjTextField3().getText();
-        if (text.equals("0")) return;
+        if(text.equals("0")){
+            return;
+        }
 
-        int lastOpIndex = -1;
-        for (int i = text.length() - 1; i >= 0; i--) {
-            if ("+-*/%".indexOf(text.charAt(i)) >= 0) {
-                lastOpIndex = i;
+        int depth = 0;
+        int start = 0;
+        for(int i = text.length() - 1; i >= 0; i--){
+            char ch = text.charAt(i);
+            if(ch == ')'){
+                depth++;
+            }else if(ch == '('){
+                depth--;
+            }else if(depth == 0 && "+-*/%".indexOf(ch) >= 0){
+                start = i + 1;
                 break;
             }
         }
-        String prefix;
-        String lastNumber;
-        if(lastOpIndex >= 0){
-            prefix = text.substring(0, lastOpIndex + 1);
+
+        String prefix = text.substring(0, start);
+        String lastNumber = text.substring(start);
+
+        if(lastNumber.isEmpty()){
+            return;
+        }
+
+        if(lastNumber.startsWith("(-") && lastNumber.endsWith(")")){
+            String inner = lastNumber.substring(2, lastNumber.length() - 1);
+            lastNumber = inner; 
         }else{
-            prefix = "";
-        }
-        if(lastOpIndex >= 0){
-            lastNumber = text.substring(lastOpIndex + 1);
-        }else{
-            lastNumber = text;
+            lastNumber = "(-" + lastNumber + ")"; 
         }
 
-        if (!lastNumber.isEmpty()) {
-            double num;
-            boolean wasP = false;
+        uif.getjTextField3().setText(prefix + lastNumber);
+    }
 
-            if(lastNumber.startsWith("(-") && lastNumber.endsWith(")")) {
-                lastNumber = lastNumber.substring(2, lastNumber.length() - 1);
-                wasP= true;
-            }
-
-            try{
-                num = Double.parseDouble(lastNumber);
-            }catch (NumberFormatException e) {
-                return;
-            }
-            
-            num = -num;
-
-            if(num < 0) {
-                lastNumber = "(-" + (-num) + ")";
-            }else{
-                lastNumber = String.valueOf(num);
-            }
-        }
-
-    uif.getjTextField3().setText(prefix + lastNumber);
-}
-
-   
     private void result() {
-    String expr = uif.getjTextField3().getText();
+        String expr = uif.getjTextField3().getText();
 
-    try {
-        double total = 0;
-        char lastAddSub = '+';
-        double lastMulDiv = 0;
-        char lastOp = ' ';
-        StringBuilder number = new StringBuilder();
+        try{
+            double total = 0;
+            char lastAddSub = '+';
+            double lastMulDiv = 0;
+            char lastOp = ' ';
+            StringBuilder number = new StringBuilder();
 
-        int i = 0;
-        if(expr.charAt(0) == '-') {
-            number.append('-');
-            i = 1;
-        }
+            int i = 0;
+            while(i < expr.length()){
+                char c = expr.charAt(i);
 
-        while (i < expr.length()) {
-            char c = expr.charAt(i);
+                if(Character.isDigit(c) || c == '.'){
+                    number.append(c);
+                    i++;
+                    continue;
+                }
 
-            if(Character.isDigit(c) || c == '.'){
-                number.append(c);
+                if(c == '%'){
+                    if(number.length() > 0){
+                        double num = Double.parseDouble(number.toString());
+                        number.setLength(0);
+
+                        int j = i + 1;
+                        StringBuilder nextNum = new StringBuilder();
+                        while(j < expr.length() && (Character.isDigit(expr.charAt(j)) || expr.charAt(j) == '.')){
+                            nextNum.append(expr.charAt(j));
+                            j++;
+                        }
+
+                        if(nextNum.length() > 0){
+                            double y = Double.parseDouble(nextNum.toString());
+                            num = (num / 100.0) * y;
+                            i = j;
+                        }else{
+                            num = num / 100.0;
+                            i++;
+                        }
+
+                        number.append(num);
+                        continue;
+                    }
+                }
+
+                if(c == '(' && i + 1 < expr.length() && expr.charAt(i + 1) == '-'){
+                    int end = expr.indexOf(')', i);
+                    if(end != -1){
+                        number.append('-').append(expr, i + 2, end);
+                        i = end + 1;
+                        continue;
+                    }
+                }
+
+                double num = Double.parseDouble(number.toString());
+                number.setLength(0);
+
+                switch (lastOp){
+                    case '*':
+                        num = lastMulDiv * num;
+                        lastOp = ' ';
+                        break;
+                    case '/':
+                        num = lastMulDiv / num;
+                        lastOp = ' ';
+                        break;
+                }
+
+                switch (c){
+                    case '*':
+                    case '/':
+                        lastOp = c;
+                        lastMulDiv = num;
+                        break;
+                    case '+':
+                    case '-':
+                        switch (lastAddSub) {
+                            case '+':
+                                total += num;
+                                break;
+                            case '-':
+                                total -= num;
+                                break;
+                        }
+                        lastAddSub = c;
+                        break;
+                }
                 i++;
-                continue;
             }
 
-            if(c == '(' && i + 1 < expr.length() && expr.charAt(i + 1) == '-') {
-                int end = expr.indexOf(')', i);
-                if (end != -1) {
-                    number.append('-').append(expr, i + 2, end);
-                    i = end + 1;
-                    continue;
+            if(number.length() > 0){
+                double num = Double.parseDouble(number.toString());
+
+                switch (lastOp){
+                    case '*':
+                        num = lastMulDiv * num;
+                        break;
+                    case '/':
+                        num = lastMulDiv / num;
+                        break;
+                }
+
+                switch (lastAddSub){
+                    case '+':
+                        total += num;
+                        break;
+                    case '-':
+                        total -= num;
+                        break;
                 }
             }
 
-            if(c == '-' && i > 0 && "+-*/%".indexOf(expr.charAt(i - 1)) >= 0) {
-                number.append('-');
-                i++;
-                continue;
-            }
+            uif.getjTextField2().setText(expr);
+            uif.getjTextField3().setText(String.valueOf(total));
+            isTypingNewNumber = true;
 
-//            if (number.length() == 0) {
-//                i++;
-//                continue;
-//            }
-
-            double num = Double.parseDouble(number.toString());
-            number.setLength(0);
-
-            switch (lastOp) {
-                case '*':
-                    num = lastMulDiv * num;
-                    lastOp = ' ';
-                    break;
-                case '/':
-                    num = lastMulDiv / num;
-                    lastOp = ' ';
-                    break;
-                case '%':
-                    num = lastMulDiv * num / 100;
-                    lastOp = ' ';
-                    break;
-            }
-
-            switch (c) {
-                case '*':
-                case '/':
-                case '%':
-                    lastOp = c;
-                    lastMulDiv = num;
-                    break;
-                case '+':
-                case '-':
-                    switch (lastAddSub) {
-                        case '+':
-                            total += num;
-                            break;
-                        case '-':
-                            total -= num;
-                            break;
-                    }
-                    lastAddSub = c;
-                    break;
-            }
-            i++;
+        }catch (Exception e){
+            uif.getjTextField3().setText("Error!");
+            isTypingNewNumber = true;
         }
-
-        if (number.length() > 0) {
-            String numStr = number.toString();
-            double num = Double.parseDouble(numStr);
-
-            switch (lastOp) {
-                case '*':
-                    num = lastMulDiv * num;
-                    break;
-                case '/':
-                    num = lastMulDiv / num;
-                    break;
-                case '%':
-                    num = lastMulDiv * num / 100;
-                    break;
-            }
-
-            switch (lastAddSub) {
-                case '+':
-                    total += num;
-                    break;
-                case '-':
-                    total -= num;
-                    break;
-            }
-        }
-
-        uif.getjTextField2().setText(expr);
-        uif.getjTextField3().setText(String.valueOf(total));
-        isTypingNewNumber = true;
-
-    } catch (Exception e) {
-        uif.getjTextField3().setText("Error!");
-        isTypingNewNumber = true;
     }
-}    
+
 }
     
